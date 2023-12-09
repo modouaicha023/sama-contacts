5348460c69c53282c71d891e7f75938f77a537fb
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-12-09 21:55:59 +0000

    Add favicon to index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="contact.png">
+
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
